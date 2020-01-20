bb8d9f4477003d752888dba757a8b97bcacc38de
zt247206112 <247206112@qq.com>
2020-01-20 15:31:22 +0800

    代码提交

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="text">{{text}}</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            text: 'abc'
+        }
+    }
+}
+</script>
+
+<style scoped>
+#text {
+    color: red;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// 入口文件
+import Vue from 'vue'
+import App from './app.vue'
+const root = document.createElement('div');
+document.body.appendChild(root);
+new Vue({
+    render: (h) => h(App)
+}).$mount(root)
