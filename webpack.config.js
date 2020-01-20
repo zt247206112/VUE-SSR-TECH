110c0c15bd0dfab4c8376ae5f792576521ea2995
zt247206112 <247206112@qq.com>
2020-01-20 15:47:15 +0800

    添加webpack配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = {
     entry: path.join(__dirname, 'src/index.js'),
     output: {
@@ -10,7 +11,39 @@ module.exports = {
             {
                 test: /.vue$/,
                 loader: 'vue-loader'
-            }
+            },
+            { // 用于处理es6
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+            { // 用于处理css
+                test: /\.m?css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require("autoprefixer")
+                            ]
+                        }
+                    }
+                ]
+            },
         ]
-    }
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ]
 }
